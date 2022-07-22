6a97342c279d1da9c5bb2aae2e006b27803d4dc8
Rizwan Shaikh <100840582+rizwan992323@users.noreply.github.com>
2022-07-22 20:27:13 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,26 @@
+//  window.alert('success')
+
+
+//string types
+var x = "Javscript";
+console.log(x);
+
+//number
+const a = 1234;
+console.log( 'Amount'  + a);
+//null
+let r = null
+console.log(r);
+
+//undefind
+let e;
+console.log(e)
+
+
+function myfunc(){
+    let
+}
+
+
+
+
