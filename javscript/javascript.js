520a39a5233cd2fbf69021ddcfe604c8fdb98359
Rizwan Shaikh <100840582+rizwan992323@users.noreply.github.com>
2022-07-28 11:41:12 +0530

    Add files via upload

---
+++ javscript/javascript.js
@@ -0,0 +1,42 @@
+//  window.alert('success')
+
+
+//string types
+var x = "Html";
+console.log(x)
+
+//number
+const a = 1234;
+console.log( 'Amount'  + a);
+//null
+let r = null
+console.log(r);
+
+//undefind
+let e;
+console.log(e)
+
+const flag1 = new Boolean(true);
+
+console.log(flag1.toString());
+// expected output: "true"
+
+const flag2 = new Boolean(1);
+
+console.log(flag2.toString());
+// expected output: "true"
+
+
+
+
+
+function myFunction(){
+    document.write("This is  my simple Function")
+}
+myFunction();
+
+function  func(){
+    document.write("Hello Javascript")
+}
+/* <button onclick=" func()" > Open</button> */
+
