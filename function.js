b0c5de15bdabb98fa66895e688dd8d7fa0e9f733
Rizwan Shaikh <100840582+rizwan992323@users.noreply.github.com>
2022-07-28 11:28:14 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,17 @@
+
+function func(){
+
+
+    
+}
+
+
+function myFunction(){
+    document.write("This is  my simple Function")
+}
+myFunction();
+
+function  func(){
+    document.write("Hello Javascript")
+}
+/* <button onclick=" func()" > Open</button> */
