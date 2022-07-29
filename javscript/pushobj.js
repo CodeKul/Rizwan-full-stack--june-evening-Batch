16e023c9faebe7fbd297c26eea64fd092fe51c1c
Rizwan Shaikh <100840582+rizwan992323@users.noreply.github.com>
2022-07-29 20:19:38 +0530

    Add files via upload

---
+++ javscript/pushobj.js
@@ -0,0 +1,23 @@
+const fruits= ['apple','mango',12,'jsh',23,'a',1233 ];
+
+console.log(fruits.push('pune'))
+console.log(fruits)
+console .log(fruits.length)
+console.log(fruits.indexOf('mango'));
+
+
+var tea =['jhon','smith','deo'];
+console .log(tea);
+tea.push('hello',2,3,4,5,{name:"Rizwan",age:25,addres:"pune"})
+console.log(tea)
+
+
+let y=[
+['jack', 24,'pune','mumbai'],
+['sara',23,24,25,'india'],
+['codekul',23,34,35,'abcd']
+['code',23,234,35,'abcd']
+['xyz',23,34,354,'abcd']
+
+];
+console.log(y[1][2])
