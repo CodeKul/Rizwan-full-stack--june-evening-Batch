ad8f1176a9c1a5a68ced41a0568e0a6c05ae993e
Rizwan Shaikh <100840582+rizwan992323@users.noreply.github.com>
2022-08-08 20:24:48 +0530

    Add files via upload

---
+++ javscript/setTime.js
@@ -0,0 +1,20 @@
+
+        setTimeout(() => {
+            console.log("this is  first")
+        }, 5000);
+
+        setTimeout(() => {
+            console.log("this is  first")
+        }, 3000);
+
+        setTimeout(() => {
+            console.log("this is  first")
+        }, 1000);
+
+        setTimeout(function () {
+            console.log('this is   last')
+        }, 4000);
+
+        setTimeout(function () {
+            console.log('this is Last')
+        }, 6000);
