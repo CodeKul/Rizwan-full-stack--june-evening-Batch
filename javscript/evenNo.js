f94bfe5eed4b43af1ad2b361dc28277987041e68
Rizwan Shaikh <100840582+rizwan992323@users.noreply.github.com>
2022-08-24 09:14:51 +0530

    Add files via upload

---
+++ javscript/evenNo.js
@@ -0,0 +1,9 @@
+const number = prompt("Enter a  number");
+// check  if the  number is even
+if (number % 2 == 0) {
+    console.log("the  number is even");
+}
+//if the number is odd
+else {
+    console.log("the number is odd");
+}
