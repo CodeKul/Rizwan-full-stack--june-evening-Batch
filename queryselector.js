4313668b437d0cb4d9067ce01469a14d4922fc42
Rizwan Shaikh <100840582+rizwan992323@users.noreply.github.com>
2022-08-30 10:08:00 +0530

    Add files via upload

---
+++ queryselector.js
@@ -0,0 +1,10 @@
+{/* <p class="para"> i am aparagraph</p>
+<p class="para"> i am aparagraph</p> */}
+
+
+
+   const xyz = document.querySelectorAll(".para");
+   for(let i=0; i < xyz.length; i++){
+    xyz[i].style.backgroundColor ="red"
+
+   }
